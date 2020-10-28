1fd4a56de438d167a90aa4559657617ee7333384
Erik Schouten <Erik@ErikSchouten.com>
2020-10-28 17:30:04 +0100

    Quintuple

--- src/main/kotlin/com/erikschouten/customclasses/tuples/Tuples.kt
+++ src/main/kotlin/com/erikschouten/customclasses/tuples/Tuples.kt
@@ -1,39 +1,19 @@
 package com.erikschouten.customclasses.tuples
 
-import java.io.Serializable
-
-/**
- * Represents a tetrad of values
- *
- * There is no meaning attached to values in this class, it can be used for any purpose.
- * Quadruple exhibits value semantics, i.e. two quadruples are equal if all four components are equal.
- * An example of decomposing it into values:
- * @sample samples.misc.Tuples.tripleDestructuring
- *
- * @param A type of the first value.
- * @param B type of the second value.
- * @param C type of the third value.
- * @param D type of the third value.
- * @property first First value.
- * @property second Second value.
- * @property third Third value.
- * @property fourth Third value.
- */
 data class Quadruple<out A, out B, out C, out D>(
         val first: A,
         val second: B,
         val third: C,
         val fourth: D
-) : Serializable {
+)
 
-    /**
-     * Returns string representation of the [Quadruple] including its [first], [second], [third] and [fourth] values.
-     */
-    override fun toString(): String = "($first, $second, $third, $fourth)"
-}
+data class Quintuple<out A, out B, out C, out D, out E>(
+    val first: A,
+    val second: B,
+    val third: C,
+    val fourth: D,
+    val fifth: E
+)
 
-/**
- * Converts this triple into a list.
- * @sample samples.misc.Tuples.tripleToList
- */
 fun <T> Quadruple<T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth)
+fun <T> Quintuple<T, T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth, fifth)
